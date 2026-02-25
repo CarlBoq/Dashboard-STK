210486d3a71c7bb787e9eb30d421d5277e52516a
Carl Boquiron <147371246+CarlBoq@users.noreply.github.com>
2026-02-25 11:57:28 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
-  base: '/',
+  base: '/Dashboard-STK/',
   esbuild: {
     jsxInject: `import React from "react"`,
   },
