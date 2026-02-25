8ecbdb6011cb3051fdc54cff6199f31ceddef516
Carl Boquiron <147371246+CarlBoq@users.noreply.github.com>
2026-02-25 11:52:19 +0800

    root fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Dashboard-STK/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   esbuild: {
     jsxInject: `import React from "react"`,
   },
